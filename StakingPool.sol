d000a95e595ee781cfaac843c0190abdeec74424
justv030-cyber <justv030@gmail.com>
2026-09-15 15:28:05 +0530

    add unstake fun and staking timr

--- StakingPool.sol
+++ StakingPool.sol
@@ -6,7 +6,30 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 contract StakingPool {
     IERC20 public stakingToken;
 
+    mapping(address => uint256) public tokens;
+
+    mapping(address => uint256) public stakingTime;
+
     constructor(address _tokenAddress) {
         stakingToken = IERC20(_tokenAddress);
+    }
+
+    function stake(uint256 _amount) public {
+        require(_amount > 0, "Please Enter Valid Amount");
+
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
+
+        stakingTime[msg.sender] = block.timestamp;
+
+        tokens[msg.sender] += _amount;
+    }
+
+    function unstake(uint256 _amount) public {
+        require(_amount > 0, "Please Enter Valid Amount");
+        require(tokens[msg.sender] >= _amount, "Insufficient Balance");
+
+        tokens[msg.sender] -= _amount;
+
+        stakingToken.transfer(msg.sender, _amount);
     }
 }
