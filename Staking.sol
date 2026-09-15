b9e9cd032035d3be7085e8c904dccf968c474a88
justv030-cyber <justv030@gmail.com>
2026-09-15 15:09:36 +0530

    add boiler plate of ERC-20

---
+++ Staking.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.7.0
+pragma solidity ^0.8.27;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Staking is ERC20, Ownable {
+    constructor(address initialOwner)
+        ERC20("staking", "STK")
+        Ownable(initialOwner)
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
